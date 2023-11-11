40d57c6e4f3cdcfaaea960d6abbabc8e5926f05f
Bence KOLONICS <bence.kolonics@imt-bs.eu>
2023-11-11 18:54:10 +0100

    Normalize values in df_large.csv

--- convert_hf.py
+++ convert_hf.py
@@ -3,7 +3,10 @@ import pandas as pd
 
 df = pd.read_csv("output/df_large.csv", index_col=0)
 
+sum_df = df.values.sum()
 
-df = df.div(df.sum(axis=1), axis=0)
-print(df)
+df = df.map(lambda x: (x * 100 / sum_df).round(2))
+
+
+
 df.to_csv('output/df_large_norm.csv', index=True, header=True, sep=',')
